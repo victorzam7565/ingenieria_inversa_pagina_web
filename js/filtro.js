57f2c87c0549709ae320bea7e9232c843335aace
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-05-26 23:46:42 -0500

    pagina ing

--- js/filtro.js
+++ js/filtro.js
@@ -9,16 +9,49 @@ $(".cont-work").not("."+valor).hide("1000");
 $(".cont-work").filter("."+valor).show("1000");
      }
    });
-   let equipo=$("#enlace-equipo").offset().top,
-    servicio=$("#enlace-servicio").offset().top,
-    trabajo=$("#enlace-trabajo").offset().top,
-    contacto=$("#enlace-contacto").offset().top;
+   let equipo=$("#equipo").offset().top,
+    servicio=$("#servicio").offset().top,
+    trabajo=$("#trabajo").offset().top,
+    contacto=$("#contacto").offset().top;
 
-    window.addEventListener("resize",funtion(){
-let equipo=$("#enlace-equipo").offset().top,
-servicio=$("#enlace-servicio").offset().top,
-    trabajo=$("#enlace-trabajo").offset().top,
-    contacto=$("#enlace-contacto").offset().top;
-
-    }
+    window.addEventListener("resize",function(){
+let equipo=$("#equipo").offset().top,
+servicio=$("#servicio").offset().top,
+    trabajo=$("#trabajo").offset().top,
+    contacto=$("#contacto").offset().top;
+    });
+    $("#enlace-inicio").on("click",function(e){
+     e.preventDefault();
+      $("html,body").animate({
+           scrollTop:0
+      },600);
+    });
 });
+    $("#enlace-equipo").on("click",function(e){
+     e.preventDefault();
+      $("html,body").animate({
+           scrollTop:equipo -100
+      },600);
+    });
+
+    $("#enlace-servicio").on("click",function(e){
+     e.preventDefault();
+      $("html,body").animate({
+           scrollTop:servicio -100
+      },600);
+    });
+    
+    $("#enlace-trabajo").on("click",function(e){
+     e.preventDefault();
+      $("html,body").animate({
+           scrollTop:trabajo -100
+      },600);
+    });
+
+    $("#enlace-contacto").on("click",function(e){
+     e.preventDefault();
+      $("html,body").animate({
+           scrollTop:contacto -100
+      },600);
+    });
+     
